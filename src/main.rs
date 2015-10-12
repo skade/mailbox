1f0f00db29668922ebab68d81577c0fa3e274f5b
Florian Gilcher <florian.gilcher@asquera.de>
2015-10-12 07:14:17 +0200

    Step 4: Read from network and echo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-use std::env::args;
+use std::net::{TcpListener, TcpStream};
+use std::io::BufReader;
+use std::io::BufRead;
+use std::io::Write;
 
 fn main() {
-    let who = args().skip(1);
-
-    match who.len() {
-        0 => println!("Hello, world!"),
-        _ => {
-            for name in who {
-                println!("Hello, {}!", name);
+    let listener = TcpListener::bind("127.0.0.1:7200").unwrap();
+   
+    // accept connections and process them, spawning a new thread for each one
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut s) => {
+                let name = read_name(&mut s);
+                write_greeting(&mut s, name);
+            }
+            Err(e) => {
+                println!("A connection failed. Error: {}", e);
             }
         }
     }
+}
+
+fn read_name(stream: &mut TcpStream) -> String {
+    let mut read_buffer = String::new();
+    let mut buffered_stream = BufReader::new(stream);
+    let res = buffered_stream.read_line(&mut read_buffer);
+    res.ok().expect("An error occured while reading!");
+    read_buffer
+}
+
+fn write_greeting(stream: &mut TcpStream, name: String) {
+    write!(stream, "Hello, {}!", name.trim());
 }
